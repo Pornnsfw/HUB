3b8ae3569b55521be14374bf84655356c2b24260
AX <pornnsfwsafe@gmail.com>
2026-09-13 08:24:26 -0700

    Create LayByLose.lua

---
+++ LoseStrat /LayByLose.lua
@@ -0,0 +1,84 @@
+TDS:RemoveIndex()
+
+TDS:Place("Soldier", -2.623168468475342, 262.99998474121094, 271.58221435546875)
+TDS:Upgrade(1)
+TDS:Ready()
+
+-- [ Wave 2 ] --
+TDS:Place("Soldier", -2.623168468475342, 262.99998474121094, 271.58221435546875)
+TDS:Upgrade(2)
+
+-- [ Wave 3 ] --
+TDS:Place("Soldier", -3.0838632583618164, 262.99998474121094, 271.7314147949219)
+TDS:Upgrade(3)
+
+-- [ Wave 4 ] --
+TDS:Place("Soldier", -3.1164636611938477, 262.99998474121094, 272.2812805175781)
+TDS:Upgrade(4)
+
+-- [ Wave 5 ] --
+TDS:Place("Assassin", -2.550779342651367, 262.99998474121094, 282.4583435058594)
+TDS:Upgrade(5)
+TDS:Place("Assassin", 2.668839454650879, 262.99998474121094, 282.76556396484375)
+
+-- [ Wave 6 ] --
+TDS:Upgrade(6)
+TDS:Place("Assassin", 2.557888984680176, 262.99998474121094, 280.81756591796875)
+TDS:Place("Assassin", -2.6355390548706055, 262.99998474121094, 280.7108154296875)
+TDS:Upgrade(8)
+
+-- [ Wave 7 ] --
+TDS:Place("Assassin", 2.508328437805176, 262.99998474121094, 279.5216369628906)
+TDS:Place("Assassin", -2.72670841217041, 262.99998474121094, 278.9090881347656)
+
+-- [ Wave 8 ] --
+TDS:Place("Assassin", 2.5100202560424805, 262.99998474121094, 278.420654296875)
+TDS:Place("Assassin", -2.677440643310547, 262.99998474121094, 276.8633728027344)
+TDS:Upgrade(12)
+TDS:Upgrade(12)
+
+-- [ Wave 9 ] --
+TDS:Upgrade(10)
+TDS:Upgrade(10)
+TDS:Upgrade(9)
+TDS:Upgrade(9)
+
+-- [ Wave 10 ] --
+TDS:Upgrade(9)
+
+-- [ Wave 11 ] --
+TDS:Upgrade(1)
+TDS:Upgrade(2)
+
+-- [ Wave 12 ] --
+TDS:Upgrade(3)
+TDS:Upgrade(4)
+TDS:Upgrade(10)
+
+-- [ Wave 13 ] --
+TDS:Upgrade(8)
+TDS:Upgrade(5)
+TDS:Upgrade(5)
+
+-- [ Wave 14 ] --
+TDS:Upgrade(6)
+TDS:Upgrade(4)
+
+-- [ Wave 15 ] --
+TDS:Upgrade(1)
+TDS:Upgrade(2)
+
+-- [ Wave 16 ] --
+TDS:Upgrade(7)
+TDS:Upgrade(7)
+TDS:VoteSkip(16)
+
+-- [ Wave 17 ] --
+TDS:VoteSkip(17)
+TDS:Upgrade(6)
+
+-- [ Wave 18 ] --
+TDS:Upgrade(7)
+TDS:VoteSkip(18)
+TDS:VoteSkip(18)
+TDS:RemoveIndex()
