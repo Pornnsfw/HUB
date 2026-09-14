017e2167ac3a3f8ebdce2b1a0bb484234dc171df
AX <pornnsfwsafe@gmail.com>
2026-09-13 21:59:15 -0700

    Create [Gems]Lose.lua

---
+++ LoseStrat /[Gems]Lose.lua
@@ -0,0 +1,161 @@
+
+TDS:GameInfo("Wretched Front", {})
+
+TDS:Place("Farm", -10.861888885498047, 1.5471417903900146, 25.09894371032715)
+TDS:Place("Farm", -10.983602523803711, 1.5101948976516724, 29.22057342529297)
+TDS:Place("Farm", -11.011951446533203, 1.4378962516784668, 33.24736022949219)
+TDS:Upgrade(3)
+TDS:Ready()
+
+-- [ Wave 1 ] --
+TDS:Upgrade(2)
+TDS:VoteSkip(1)
+TDS:Upgrade(1)
+
+-- [ Wave 2 ] --
+TDS:VoteSkip(2)
+TDS:Upgrade(1)
+
+-- [ Wave 3 ] --
+TDS:Upgrade(2)
+TDS:VoteSkip(3)
+TDS:Place("Boomerang", 6.083185195922852, 1.303847074508667, 22.02817153930664)
+
+-- [ Wave 4 ] --
+TDS:Upgrade(4)
+TDS:VoteSkip(4)
+TDS:Upgrade(3)
+
+-- [ Wave 5 ] --
+TDS:Place("Farm", -11.129362106323242, 1.339637279510498, 37.122920989990234)
+TDS:Upgrade(5)
+TDS:VoteSkip(5)
+TDS:Upgrade(5)
+
+-- [ Wave 6 ] --
+TDS:Upgrade(4)
+TDS:VoteSkip(6)
+TDS:Upgrade(1)
+
+-- [ Wave 7 ] --
+TDS:Upgrade(2)
+TDS:VoteSkip(7)
+
+-- [ Wave 8 ] --
+TDS:Upgrade(3)
+TDS:VoteSkip(8)
+TDS:Upgrade(5)
+
+-- [ Wave 9 ] --
+TDS:Upgrade(4)
+TDS:VoteSkip(9)
+
+-- [ Wave 10 ] --
+TDS:Place("Crook Boss", 6.515737533569336, 1.3898032903671265, 28.200634002685547)
+TDS:VoteSkip(10)
+TDS:Upgrade(6)
+
+-- [ Wave 11 ] --
+TDS:VoteSkip(11)
+TDS:Upgrade(6)
+
+-- [ Wave 12 ] --
+TDS:Place("Farm", -14.391939163208008, 1.5783648490905762, 25.364171981811523)
+TDS:Upgrade(7)
+TDS:Upgrade(7)
+TDS:VoteSkip(12)
+TDS:Upgrade(7)
+TDS:Place("Farm", -14.78342056274414, 1.5662105083465576, 29.22341537475586)
+TDS:Upgrade(8)
+
+-- [ Wave 13 ] --
+TDS:Upgrade(8)
+TDS:Upgrade(8)
+TDS:Place("Farm", -14.550613403320312, 1.5082982778549194, 33.150047302246094)
+TDS:Upgrade(9)
+TDS:Upgrade(9)
+TDS:VoteSkip(13)
+TDS:Upgrade(9)
+TDS:Place("Farm", -15.045982360839844, 1.3754749298095703, 37.07466506958008)
+
+-- [ Wave 14 ] --
+TDS:Upgrade(10)
+TDS:Upgrade(10)
+TDS:Upgrade(10)
+TDS:Place("Farm", -11.096109390258789, 1.2094264030456543, 41.570556640625)
+TDS:Upgrade(11)
+TDS:Upgrade(11)
+TDS:Upgrade(11)
+TDS:VoteSkip(14)
+TDS:Place("Farm", -14.696542739868164, 1.1648077964782715, 41.021297454833984)
+
+-- [ Wave 15 ] --
+TDS:Upgrade(12)
+TDS:Upgrade(12)
+TDS:Upgrade(12)
+TDS:VoteSkip(15)
+TDS:Place("Crook Boss", 6.515737533569336, 1.3753252029418945, 31.24063491821289)
+TDS:Upgrade(13)
+
+-- [ Wave 16 ] --
+TDS:Upgrade(13)
+TDS:Place("Boomerang", 9.028313636779785, 1.3163073062896729, 22.668027877807617)
+TDS:Upgrade(14)
+TDS:Upgrade(14)
+TDS:VoteSkip(16)
+TDS:Upgrade(14)
+
+-- [ Wave 17 ] --
+TDS:Place("Crook Boss", 3.475722312927246, 1.4181606769561768, 28.200637817382812)
+TDS:Upgrade(15)
+TDS:Upgrade(15)
+TDS:VoteSkip(17)
+
+-- [ Wave 18 ] --
+TDS:Place("Crook Boss", 3.4757375717163086, 1.425084114074707, 31.24063491821289)
+TDS:Upgrade(16)
+TDS:Upgrade(16)
+TDS:VoteSkip(18)
+
+-- [ Wave 19 ] --
+TDS:Place("Boomerang", 8.116286277770996, 1.030413031578064, 19.7680606842041)
+TDS:Upgrade(17)
+TDS:Upgrade(17)
+TDS:Upgrade(17)
+TDS:Place("Crook Boss", 11.620108604431152, 1.2113935947418213, 29.66733741760254)
+TDS:Upgrade(18)
+TDS:VoteSkip(19)
+
+-- [ Wave 20 ] --
+TDS:Upgrade(18)
+TDS:Place("EvolvedKingpin", 6.429534912109375, 1.3707666397094727, 34.27941131591797)
+TDS:VoteSkip(20)
+
+-- [ Wave 21 ] --
+TDS:VoteSkip(21)
+
+-- [ Wave 22 ] --
+TDS:Upgrade(6)
+TDS:VoteSkip(22)
+
+-- [ Wave 23 ] --
+TDS:VoteSkip(23)
+
+-- [ Wave 24 ] --
+TDS:Upgrade(15)
+TDS:VoteSkip(24)
+
+-- [ Wave 25 ] --
+TDS:Upgrade(13)
+TDS:VoteSkip(25)
+
+-- [ Wave 26 ] --
+TDS:VoteSkip(26)
+TDS:Upgrade(4)
+
+-- [ Wave 27 ] --
+TDS:VoteSkip(27)
+TDS:Upgrade(16)
+
+-- [ Wave 28 ] --
+TDS:VoteSkip(28)
