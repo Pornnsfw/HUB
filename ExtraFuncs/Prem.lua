d0f27550690f60acf740fbabc01f68641adceddf
AX <pornnsfwsafe@gmail.com>
2026-09-15 18:07:27 -0700

    Create Prem.lua

---
+++ ExtraFuncs/Prem.lua
@@ -0,0 +1,370 @@
+
+--!strict
+-- AutoTrialsRequirements.lua
+-- Static trial/fallback requirements and strategy script URLs.
+
+return {
+    trialScripts = {
+        ["Speedy Enemies"]    = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/PremiumTrials/Speedy.lua", -- done
+        ["Glass"]             = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/PremiumTrials/Glass.lua",
+        ["Quarantine"]        = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/PremiumTrials/Quarantine.lua",
+        ["Fog"]               = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/PremiumTrials/Fog.lua",
+        ["Limitation"]        = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/PremiumTrials/Limitation.lua", -- done
+        ["Flying Enemies"]    = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/PremiumTrials/Flying.lua", -- done
+        ["Jailed"]            = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/PremiumTrials/Jailed.lua", -- done
+        ["Exploding Enemies"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/PremiumTrials/Exploading.lua", -- done
+        ["Inflation"]         = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/PremiumTrials/Inflation.lua", -- done 
+        ["Committed"]         = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/PremiumTrials/Committed.lua", -- done
+        ["Hidden Enemies"]    = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/PremiumTrials/Hidden.lua", -- done
+        ["Broke"]             = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/PremiumTrials/Broke.lua", -- done 
+        ["Healthy Enemies"]   = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/PremiumTrials/Healthy.lua", -- done
+    },
+	
+    fallbackScripts = {
+        ["Easy"]         = "https://raw.githubusercontent.com/AmonguszzZ/ModdedAether/refs/heads/main/Strats/Mode.lua",
+        ["Casual"]       = "https://raw.githubusercontent.com/AmonguszzZ/ModdedAether/refs/heads/main/Strats/Mode.lua",
+        ["Intermediate"] = "https://raw.githubusercontent.com/AmonguszzZ/ModdedAether/refs/heads/main/Strats/Mode.lua",
+        ["Molten"]       = "https://raw.githubusercontent.com/AmonguszzZ/ModdedAether/refs/heads/main/Strats/Mode.lua",
+        ["Fallen"]       = "https://raw.githubusercontent.com/AmonguszzZ/ModdedAether/refs/heads/main/Strats/Mode.lua",
+        ["Frost"]        = "https://raw.githubusercontent.com/AmonguszzZ/ModdedAether/refs/heads/main/Strats/Mode.lua",
+    },
+
+    trialConfigs = {
+        ["Speedy Enemies"] = {
+            Level = 175,
+            Towers = {"Tesla", "Gatling Gun", "Medic", "Mercenary Base", "Trapper"}, -- no skill tree, no gold, no hardcore
+            Golden = {},
+            SkillTree = {},
+        },
+        ["Glass"] = {
+            Level = 175,
+            Towers = {"Hacker", "Gatling Gun", "Militant", "Mercenary Base", "Trapper"},
+            Golden = {},
+            SkillTree = {},
+        },
+        ["Quarantine"] = {
+            Level = 175,
+            Towers = {"Hacker", "Gatling Gun", "Militant", "Mercenary Base", "DJ Booth"},
+            Golden = {},
+            SkillTree = {},
+        },
+        ["Fog"] = {
+            Level = 175,
+            Towers = {"Trapper", "Hacker", "Gatling Gun", "Mercenary Base", "DJ Booth"},
+            Golden = {},
+            SkillTree = {},
+        },
+        ["Limitation"] = {
+            Level = 175,
+            Towers = {"Trapper", "Medic", "Gatling Gun", "Mercenary Base", "DJ Booth"}, -- no gold, no skill tree, no hardcore
+            Golden = {},
+            SkillTree = {},
+        },
+        ["Flying Enemies"] = {
+            Level = 175,
+            Towers = {"Militant", "Gatling Gun", "Medic", "Mercenary Base", "DJ Booth"}, -- no gold, no skill tree, no hardcore
+            Golden = {},
+            SkillTree = {},
+        },
+        ["Jailed"] = {
+            Level = 175,
+            Towers = {"Scout", "Gatling Gun", "Militant", "Mercenary Base", "Paintballer", "Assassin", "DJ Booth", "Crook Boss"}, -- no gold, no skill tree, no hardcore, - higher winrate chance
+            Golden = {},
+            SkillTree = {},
+        },
+        ["Exploding Enemies"] = {
+            Level = 175,
+            Towers = {"Militant", "Gatling Gun", "Medic", "Mercenary Base", "DJ Booth"}, -- no gold, no skill tree, no hardcore
+            Golden = {},
+            SkillTree = {},
+        },
+        ["Inflation"] = {
+            Level = 175,
+            Towers = {"Ace Pilot", "Trapper", "Gatling Gun", "DJ Booth", "Medic"}, -- no gold, no skill tree, no hardcore
+            Golden = {},
+            SkillTree = {},
+        },
+        ["Committed"] = {
+            Level = 175,
+            Towers = {"Hacker", "Gatling Gun", "Medic", "Scout", "Demoman"}, -- super exepsive 50% winrate
+            Golden = {"Scout", "Demoman"},
+            SkillTree = {
+                ["Bigger Budget"] = 25,
+                ["Fortify"] = 40,
+                ["Stonks"] = 20,
+                ["Over-Heal"] = 25,
+                ["Bandages"] = 25,
+                ["Accelerator"] = 25,
+                ["Enhanced Optics"] = 20,
+                ["Scavenger"] = 20,
+				["Improved Gunpowder"] = 25,
+				["Fight Dirty"] = 25,
+				["Precision"] = 15,
+				["Re-enforcements"] = 10,
+				["Extreme Conditioning"] = 25,
+            },
+        },
+        ["Hidden Enemies"] = {
+            Level = 175,
+            Towers = {"Gatling Gun", "Medic", "Mercenary Base", "Militant", "DJ Booth"}, -- no gold, no skill tree, no hardcore
+            Golden = {},
+            SkillTree = {},
+        },
+        ["Broke"] = {
+            Level = 175,
+            Towers = {"Gatling Gun", "Trapper", "Militant", "Trapper", "DJ Booth", "Assassin"}, -- no gold, no skill tree, no hardcore
+            Golden = {},
+            SkillTree = {},
+        },
+        ["Healthy Enemies"] = {
+            Level = 175,
+            Towers = {"Ace Pilot", "Mercenary Base", "DJ Booth", "Gatling Gun", "Medic"}, -- No Gold, No Hardcore
+            Golden = {},
+            SkillTree = {
+                ["Bigger Budget"] = 10,
+                ["Fortify"] = 10,
+                ["Stonks"] = 10,
+                ["Over-Heal"] = 10,
+                ["Bandages"] = 10,
+                ["Accelerator"] = 10,
+                ["Enhanced Optics"] = 10,
+                ["Resourcefulness"] = 10,
+            },
+        },
+    },
+
+    -- Standard modes do not have a trial title, so fallback requirements stay separate.
+   FallbackConfigs = {
+    ["Easy"] = {
+        Level = 175,
+        Towers = {"Gatling Gun", "Trapper", "Medic", "DJ Booth", "Mercenary Base"},
+        Golden = {},
+        SkillTree = {},
+        Maps = {"Lay By", "Simplicity"},
+        Scripts = {
+            ["Lay By"] = "https://raw.githubusercontent.com/AmonguszzZ/ModdedAether/refs/heads/main/Strats/Mode.lua",
+            ["Simplicity"] = "https://raw.githubusercontent.com/AmonguszzZ/ModdedAether/refs/heads/main/Strats/Mode.lua",
+        },
+    },
+    ["Casual"] = {
+        Level = 175,
+        Towers = {"Gatling Gun", "Trapper", "Medic", "DJ Booth", "Mercenary Base"},
+        Golden = {},
+        SkillTree = {},
+        Maps = {"Lay By", "Simplicity"},
+        Scripts = {
+            ["Lay By"] = "https://raw.githubusercontent.com/AmonguszzZ/ModdedAether/refs/heads/main/Strats/Mode.lua",
+            ["Simplicity"] = "https://raw.githubusercontent.com/AmonguszzZ/ModdedAether/refs/heads/main/Strats/Mode.lua",
+        },
+    },
+    ["Intermediate"] = {
+        Level = 175,
+        Towers = {"Gatling Gun", "Trapper", "Medic", "DJ Booth", "Mercenary Base"},
+        Golden = {},
+        SkillTree = {},
+        Maps = {"Simplicity", "Lay By"},
+        Scripts = {
+            ["Lay By"] = "https://raw.githubusercontent.com/AmonguszzZ/ModdedAether/refs/heads/main/Strats/Mode.lua",
+            ["Simplicity"] = "https://raw.githubusercontent.com/AmonguszzZ/ModdedAether/refs/heads/main/Strats/Mode.lua",
+        },
+    },
+    ["Molten"] = {
+        Level = 175,
+        Towers = {"Gatling Gun", "Trapper", "Medic", "DJ Booth", "Mercenary Base"},
+        Golden = {},
+        SkillTree = {},
+        Maps = { "Lay By"},
+        Scripts = {
+		    ["Lay By"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/WinStrat/MoltenLayby.lua",
+        },
+    },
+    ["Fallen"] = {
+        Level = 175,
+        Towers = {"Gatling Gun", "Trapper", "Medic", "DJ Booth", "Mercenary Base"},
+        Golden = {},
+        SkillTree = {},
+        Maps = {"Lay By"},
+        Scripts = {
+            ["Lay By"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/WinStrat/FallenLayby.lua",
+        },
+    },
+    ["Frost"] = {
+        Level = 175,
+        Towers = {"Gatling Gun", "Trapper", "Medic", "DJ Booth", "Mercenary Base"},
+        Golden = {},
+        SkillTree = {},
+        Maps = {"Lay By"},
+        Scripts = {
+            ["Lay By"] = "https://raw.githubusercontent.com/AmonguszzZ/ModdedAether/refs/heads/main/Strats/Mode.lua",
+        },
+    },
+},
+    allTrialOptions = {
+        "Exploding Enemies",
+        "Fog",
+        "Quarantine",
+        "Speedy Enemies",
+        "Glass",
+        "Limitation",
+        "Flying Enemies",
+        "Jailed",
+        "Inflation",
+        "Committed",
+        "Hidden Enemies",
+        "Broke",
+        "Healthy Enemies",
+    },
+
+    fallbackModesList = {
+        "Molten",
+        "Fallen",
+    },
+
+	CrateConfigs = {
+        Coins = {
+            Lose = {
+                Level = 15,
+                Mode = "Molten",
+                Towers = {"Assassin", "Soldier"},
+                Golden = {},
+                SkillTree = {},
+                Maps = {"Simplicity", "Winter Abyss"},
+                Scripts = {
+                    ["Simplicity"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/LoseStrat%20/SimplicityLose.lua",
+					["Winter Abyss"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/LoseStrat%20/WinterAbyssLose.lua",
+                },
+            },
+            Win = {
+                Level = 175,
+                Mode = "Fallen",
+                Towers = {"Gatling Gun", "Trapper", "Medic", "Mercenary Base", "DJ Booth"},
+                Golden = {},
+                SkillTree = {},
+                Maps = {"Lay By", "Simplicity"},
+                Scripts = {
+                    ["Lay By"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/WinStrat/FallenLayby.lua",
+                    ["Simplicity"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/WinStrat/FallenLayby.lua",
+                },
+            },
+        },
+        Gems = {
+            Lose = {
+                Level = 50,
+                Mode = "hardcore",
+                Towers = {"Farm", "Boomerang", "Crook Boss"},
+                Golden = {},
+                SkillTree = {},
+                Maps = {"Wretched Front"},
+                Scripts = {
+                    ["Wretched Front"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/LoseStrat%20/%5BGems%5DLose.lua",
+                },
+            },
+            Win = {
+                Level = 175,
+                Mode = "hardcore",
+                Towers = {"Gatling Gun", "Pyromancer", "Medic", "Mercenary Base", "Hacker"},
+                Golden = {"Pyromancer"},
+                SkillTree = {},
+                Maps = {"Wretched Front"},
+                Scripts = {
+                    ["Wretched Front"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/WinStrat/FallenLayby.lua",
+                },
+            },
+        },
+    },
+
+	AutoEvoConfigs = {
+	   Coins = {
+        Lose = {
+            Level = 50,
+            Mode = "Molten",
+            Golden = {},
+            SkillTree = {},
+            Maps = {"Simplicity", "Winter Abyss"},
+            
+            -- You can now customize the Towers for each specific Evo target!
+            Towers = {
+                ["Scout"] = {"Assassin", "Soldier"},
+                ["Shotgunner"] = {"Assassin", "Soldier"},
+                ["Crook Boss"] = {"Assassin", "Soldier"},
+                ["Minigunner"] = {"Assassin", "Soldier"}
+            },
+            
+            -- Dynamic scripts based on the active tower you are farming
+            Scripts = {
+                ["Scout"] = {
+                    ["Simplicity"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/LoseStrat%20/SimplicityLose.lua",
+                    ["Winter Abyss"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/LoseStrat%20/WinterAbyssLose.lua",
+                },
+                ["Shotgunner"] = {
+                    ["Simplicity"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/LoseStrat%20/SimplicityLose.lua",
+                    ["Winter Abyss"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/LoseStrat%20/WinterAbyssLose.lua",
+                },
+                ["Crook Boss"] = {
+                    ["Simplicity"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/LoseStrat%20/SimplicityLose.lua",
+                    ["Winter Abyss"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/LoseStrat%20/WinterAbyssLose.lua",
+                },
+                ["Minigunner"] = {
+                    ["Simplicity"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/LoseStrat%20/SimplicityLose.lua",
+                    ["Winter Abyss"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/LoseStrat%20/WinterAbyssLose.lua",
+                }
+            }
+        },
+        Win = {
+            Level = 50,
+            Mode = "Fallen",
+            Golden = {},
+            SkillTree = {},
+            Maps = {"Lay By"},
+            Towers = {
+                ["Scout"] = {"Gatling Gun", "Trapper", "Hacker"},
+                ["Shotgunner"] = {"Gatling Gun", "Trapper", "Hacker"},
+                ["Crook Boss"] = {"Gatling Gun", "Trapper", "Hacker"},
+                ["Minigunner"] = {"Gatling Gun", "Trapper", "Hacker"}
+            },
+            Scripts = {
+                ["Scout"] = {
+                    ["Lay By"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/Evo/Win/OperatorLayby.lua",
+                },
+                ["Shotgunner"] = {
+                    ["Lay By"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/Evo/Win/EnforcerLayby.lua",
+                },
+                ["Crook Boss"] = {
+                    ["Lay By"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/Evo/Win/KingpinLayby.lua",
+                },
+                ["Minigunner"] = {
+                    ["Lay By"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/Evo/Win/JuggernautLayby.lua",
+                }
+            }
+        },
+    },
+    Gems = {
+        Lose = {
+            Level = 50,
+            Mode = "hardcore",
+            Golden = {},
+            SkillTree = {},
+            Maps = {"Wretched Front"},
+            Towers = {
+                ["Scout"] = {"Farm", "Boomerang", "Crook Boss"},
+                ["Shotgunner"] = {"Farm", "Boomerang", "Crook Boss"},
+                ["Crook Boss"] = {"Farm", "Boomerang", "Crook Boss"},
+                ["Minigunner"] = {"Farm", "Boomerang", "Crook Boss"}
+            },
+            Scripts = {
+                ["Scout"] = {
+                    ["Wretched Front"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/LoseStrat%20/%5BGems%5DLose.lua",
+                },
+                ["Shotgunner"] = {
+                    ["Wretched Front"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/LoseStrat%20/%5BGems%5DLose.lua",
+                },
+                ["Crook Boss"] = {
+                    ["Wretched Front"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/LoseStrat%20/%5BGems%5DLose.lua",
+                },
+                ["Minigunner"] = {
+                    ["Wretched Front"] = "https://raw.githubusercontent.com/Pornnsfw/HUB/refs/heads/main/LoseStrat%20/%5BGems%5DLose.lua",
+                }
+            }
+        }
+    }
+}
