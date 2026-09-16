ce3ecffc6a20cb076bc33042491df7c4c109f709
AX <pornnsfwsafe@gmail.com>
2026-09-16 08:46:17 -0700

    Update OperatorLose.lua

--- Evo/Lose/OperatorLose.lua
+++ Evo/Lose/OperatorLose.lua
@@ -1,6 +1,6 @@
 local Globals = getgenv()
 Globals.AutoSkip = true
-TDS:Loadout("Trapper", "Gatling Gun", "Hacker", "Scout", "EvolvedOperator")
+TDS:Loadout("Assassin", "Soldier", "", "Scout", "EvolvedOperator")
 
 TDS:RemoveIndex()
 TDS:Place("Assassin", -20.472789764404297, 22.99998474121094, -12.017228126525879)
