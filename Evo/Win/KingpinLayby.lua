961646a3c1eb67b079c6eb037710f2d909d349b8
AX <pornnsfwsafe@gmail.com>
2026-09-15 17:42:50 -0700

    Update KingpinLayby.lua

--- Evo/Win/KingpinLayby.lua
+++ Evo/Win/KingpinLayby.lua
@@ -1,3 +1,8 @@
+local Globals = getgenv()
+Globals.AutoGatling = true
+Globals.AutoRejoin = true
+Globals.AutoMercenary = true
+
 TDS:Loadout("Trapper", "Gatling Gun", "Hacker", "Crook Boss", "EvolvedKingpin")
 TDS:GameInfo("Lay By", {HiddenEnemies = true, Committed = true, SpeedyEnemies = true, Limitation = true, Quarantine = true, ExplodingEnemies = true, Fog = true})
 
