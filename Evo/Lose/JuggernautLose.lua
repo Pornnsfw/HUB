48cc16ad3303ea3cb60e7e8354bc62058193a8aa
AX <pornnsfwsafe@gmail.com>
2026-09-16 08:45:33 -0700

    Update JuggernautLose.lua

--- Evo/Lose/JuggernautLose.lua
+++ Evo/Lose/JuggernautLose.lua
@@ -1,6 +1,6 @@
 local Globals = getgenv()
 Globals.AutoSkip = true
-TDS:Loadout("Trapper", "Gatling Gun", "Hacker", "Minigunner", "EvolvedJuggernaut")
+TDS:Loadout("Assassin", "Soldier", "", "Minigunner", "EvolvedJuggernaut")
 
 TDS:RemoveIndex()
 TDS:Place("Assassin", -20.472789764404297, 22.99998474121094, -12.017228126525879)
